046e48d4da795bc7eaed112c8fc7009120e29eb0
amirwahla <39020081+amirwahla@users.noreply.github.com>
2022-08-16 09:39:51 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+const clientId = 'your client id';
+import { gapi } from 'gapi-script';
+
+function App() {
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+  const onLoginSuccess = (res) => {
+    console.log('Login Success:', res.profileObj);
+    setShowloginButton(false);
+    setShowlogoutButton(true);
+  };
+
+  const onLoginFailure = (res) => {
+    console.log('Login Failed:', res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert('You have been logged out successfully');
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
+  };
+
+  return (
+    <div>
+      {showloginButton ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign In"
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+        />
+      ) : null}
+
+      {showlogoutButton ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Sign Out"
+          onLogoutSuccess={onSignoutSuccess}></GoogleLogout>
+      ) : null}
+    </div>
+  );
+}
+export default App;
